1f402775500f420cf1d6d66cf27a88e82f4575c1
Justin Ricks <justin.rixx@gmail.com>
2015-06-26 19:24:44 -0500

    Finish UI and add error checking to EditCategoryActivity

--- app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivity.java
+++ app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivity.java
@@ -2,16 +2,20 @@ package com.gmail.rixx.justin.envelopebudget;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import com.gmail.rixx.justin.envelopebudget.DataObjects.Category;
 
 
 public class EditCategoryActivity extends AppCompatActivity {
@@ -28,12 +32,61 @@ public class EditCategoryActivity extends AppCompatActivity {
             setUpToolbar();
         }
 
-        /*
-        // get the save the date with the data
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
-        String formattedDate = df.format(c.getTime());
-        */
+        setOnClickListeners();
+    }
+
+    /**
+     * set an onclick on the FAB. Get all the data and get ready to save it
+     */
+    private void setOnClickListeners() {
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = ((EditText) findViewById(R.id.name_edittext)).getText().toString();
+                String amountString = ((EditText) findViewById(R.id.amount_edittext)).getText().toString();
+                String date = ((TextView) findViewById(R.id.date_textview)).getText().toString();
+
+                // not sure what the choice was
+                Category.RefreshCode refreshCode;
+
+                // check the radio buttons
+                if (((RadioButton) findViewById(R.id.month_radiobutton)).isChecked()) {
+                    refreshCode = Category.RefreshCode.MONTHLY;
+                } else {
+                    refreshCode = Category.RefreshCode.BIWEEKLY;
+                }
+
+
+                TextInputLayout nameLayout = (TextInputLayout) findViewById(R.id.input_layout_name);
+                TextInputLayout amountLayout = (TextInputLayout) findViewById(R.id.input_layout_dollar);
+
+                // do some error checking
+                if (name.equals("")) {
+                    nameLayout.setErrorEnabled(true);
+                    nameLayout.setError("Please enter a name for the category");
+
+                    return;
+                } else {
+                    nameLayout.setErrorEnabled(false);
+                }
+                if (amountString.equals("")) {
+                    amountLayout.setErrorEnabled(true);
+                    amountLayout.setError("Please enter the allocated amount");
+
+                    return;
+                } else {
+                    amountLayout.setErrorEnabled(false);
+                }
+
+                // get the double out
+                double amount = Double.valueOf(amountString);
+
+                Category c = new Category(name, amount, date, refreshCode);
+
+                Snackbar.make(mToolbar, "Got the data", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void setUpToolbar() {
--- app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivityFragment.java
+++ app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivityFragment.java
@@ -1,10 +1,19 @@
 package com.gmail.rixx.justin.envelopebudget;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 /**
@@ -12,12 +21,61 @@ import android.view.ViewGroup;
  */
 public class EditCategoryActivityFragment extends Fragment {
 
+    private TextView date;
+
     public EditCategoryActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_category, container, false);
+        View v = inflater.inflate(R.layout.fragment_edit_category, container, false);
+
+        date = (TextView) v.findViewById(R.id.date_textview);
+        setOnClickListeners();
+
+        return v;
+    }
+
+    private void setOnClickListeners() {
+
+        // set the textview to today
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
+        String formattedDate = df.format(c.getTime());
+
+        date.setText(formattedDate);
+
+        // set an onclick listener
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment picker = new DatePickerFragment();
+                picker.show(getFragmentManager(), "Date Picker");
+            }
+        });
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // save the user's choice
+            ((TextView) getActivity().findViewById(R.id.date_textview))
+                    .setText(String.format("%02d", month + 1) + "-" + String.format("%02d", day)
+                            + "-" + String.valueOf(year));
+        }
     }
 }
