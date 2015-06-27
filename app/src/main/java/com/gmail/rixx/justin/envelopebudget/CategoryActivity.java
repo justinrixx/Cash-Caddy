e587a49a2977e4cd3ef7e8ee59029d3f97160661
Justin Ricks <justin.rixx@gmail.com>
2015-06-26 19:38:50 -0500

    Remove all contextual menus and add colors to the navigationview

--- app/src/main/java/com/gmail/rixx/justin/envelopebudget/CategoryActivity.java
+++ app/src/main/java/com/gmail/rixx/justin/envelopebudget/CategoryActivity.java
@@ -2,12 +2,9 @@ package com.gmail.rixx.justin.envelopebudget;
 
 import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -46,27 +43,5 @@ public class CategoryActivity extends AppCompatActivity {
             collapsingToolbar.setTitle(getIntent().getStringExtra(getString(R.string.intent_extra_category_name)) + " : $120.00");
             collapsingToolbar.setContentScrimColor(getResources().getColor(R.color.primary));
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_category, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivity.java
+++ app/src/main/java/com/gmail/rixx/justin/envelopebudget/EditCategoryActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -101,28 +99,5 @@ public class EditCategoryActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(getResources().getColor(R.color.primary_dark));
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_category, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
