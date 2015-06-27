4718af1752d03072fa3fbfb254cf9a3fdd74aa37
Justin Ricks <justin.rixx@gmail.com>
2015-06-26 19:45:21 -0500

    Add elevation to FABs and Toolbars

--- app/src/main/java/com/gmail/rixx/justin/envelopebudget/CategoryActivity.java
+++ app/src/main/java/com/gmail/rixx/justin/envelopebudget/CategoryActivity.java
@@ -12,7 +12,6 @@ import android.view.WindowManager;
 public class CategoryActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-    private CollapsingToolbarLayout collapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +29,13 @@ public class CategoryActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
             // set the title
-            //getSupportActionBar().setTitle(getIntent().getStringExtra(getString(R.string.intent_extra_category_name)));
+            getSupportActionBar().setTitle(getIntent().getStringExtra(getString(R.string.intent_extra_category_name)));
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 Window window = getWindow();
                 window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 window.setStatusBarColor(getResources().getColor(R.color.primary_dark));
             }
-
-            // set up the collapsing toolbar
-            collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-            collapsingToolbar.setTitle(getIntent().getStringExtra(getString(R.string.intent_extra_category_name)) + " : $120.00");
-            collapsingToolbar.setContentScrimColor(getResources().getColor(R.color.primary));
         }
     }
 }
